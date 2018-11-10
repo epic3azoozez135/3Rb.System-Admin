6a4fea091384468ceb7ab4ef9c303f5ac19d90ef
zMishaery <44931500+zMishaeryy@users.noreply.github.com>
2018-11-11 01:25:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -95,23 +95,6 @@ client.on('message', message => {
 
 
 
-//كود اي احد يرسل لي البوت يقولي
-client.on('message', function(message) {
-    if (message.channel.type === "dm") {
-        if (message.author.id === client.user.id) return;
-        var stewart = new Discord.RichEmbed()
-            .setColor('RANDOM')
-            .setTimestamp()
-            .setTitle('``رساله جديده في خاص البوت``')
-            .setThumbnail(`${message.author.avatarURL}`)
-            .setDescription(`\n\n\`\`\`${message.content}\`\`\``)
-            .setFooter(`من (@${message.author.tag})  |  (${message.author.id})`)
-        client.channels.get("507276039571898378").send({ embed: stewart });
-    }
-});
-
-
-
 //كودي يوريك سكنك في ماين كرافت
 client.on("message", message => {
     var prefix = "#"
