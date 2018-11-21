516566d175fb6ec57cd1a55ffc01b753c2946c36
zMishaery <44931500+zMishaeryy@users.noreply.github.com>
2018-11-21 15:27:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -97,7 +97,7 @@ client.on('message', message => {
 
 //كود يجبلك عدد واسامي. السيرفرات الي موجود فيها بوتك مع ذكر عدد الاشخاص الي في السيرفر 
 client.on('message', message => {
-       if (message.content.startsWith(prefix + '-server')) {
+       if (message.content.startsWith(prefix + 'server')) {
      let msg =  client.guilds.map(guild => `**${guild.name}** **عدد الاعضاء :** ${guild.memberCount}`).join('\n');
   let embed = new Discord.RichEmbed()
   .setTitle(`${client.guilds.size} **:** **سيرفرات** `)
@@ -123,7 +123,16 @@ client.on('message', msg => {
 
 
 
-
+client.on('guildMemberAdd', member => {
+  member.guild.fetchInvites().then(guildInvites => {
+    const ei = invites[member.guild.id];
+    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+    const inviter = client.users.get(invite.inviter.id);
+    const stewart = member.guild.channels.find("name", "3rb");
+     stewart.send(`<@${member.user.id}> تمت الدعوه من <@${inviter.id}>`);
+   //  stewart.send(`<@${member.user.id}> joined using invite code ${invite.code} from <@${inviter.id}>. Invite was used ${invite.uses} times since its creation.`);
+  });
+});
 
 
 
