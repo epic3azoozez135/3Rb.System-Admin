37ce2c7f13c6ec426bf93fd3428fb9691924d641
zMishaery <44931500+zMishaeryy@users.noreply.github.com>
2018-11-28 23:01:23 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -78,7 +78,7 @@ client.on('message', message => { if(!message.channel.guild) return; if(message.
 ////////////////////////////////////////////////////////////////////////////
 
 //كود الباند
-var prefix = "#"
+var prefix = "#";
 client.on('message', message => {
   if (message.author.x5bz) return;
   if (!message.content.startsWith(prefix)) return;
