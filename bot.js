0222bbf8e94473c2a310b52c774154eaa935e09d
zMishaery <44931500+zMishaeryy@users.noreply.github.com>
2018-11-29 16:59:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -254,7 +254,16 @@ msg.delete();
 ////////////////////////////////////////////////////////////////////////////
 
 //كود الحذف الروم او الشات
-
+client.on("message", (message) => { // Last Code
+    if (message.content.startsWith('#del')) { // الامر والبريفكس
+        if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.reply("You Don't Have `MANAGE_CHANNELS` Premissions "); // Last Code
+ 
+        let args = message.content.split(' ').slice(1); // Last Code
+        let channel = message.client.channels.find('name', args.join(' ')); // Last Code
+        if (!channel) return message.reply('**لا يوجد اي روم بهذا الاسم**').catch(console.error);
+        channel.delete() // Last Code
+    }
+});
 
 ////////////////////////////////////////////////////////////////////////////
 
@@ -775,50 +784,7 @@ client.on('message', message => {
   }
   });
 
-//يقول كم البوت شغال
-client.on('message', message => {
-  var prefix = "#" // البريفكس
-       if (message.author.bot) return;
-  if (message.content.startsWith(prefix + "شغال")) { // الامر
-      let uptime = client.uptime;
-   
-      let days = 0;
-      let hours = 0;
-      let minutes = 0;
-      let seconds = 0;
-      let notCompleted = true;
-   
-      while (notCompleted) {
-   
-          if (uptime >= 8.64e+7) {
-   
-              days++;
-              uptime -= 8.64e+7;
-   
-          } else if (uptime >= 3.6e+6) {
-   
-              hours++;
-              uptime -= 3.6e+6;
-   
-          } else if (uptime >= 60000) {
-   
-              minutes++;
-              uptime -= 60000;
-   
-          } else if (uptime >= 1000) {
-              seconds++;
-              uptime -= 1000;
-   
-          }
-   
-          if (uptime < 1000)  notCompleted = false;
-   
-      }
-   
-      message.channel.send(" +${days} days, ${hours} hrs, ${minutes} , ${seconds} sec+ ");
-   
-  }
-  });
+
 
 ////////////////////////////////////////////////////////////////////////////
 
