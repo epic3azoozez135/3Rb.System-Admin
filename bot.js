f9b43560369773c1e6d87a33e1aaa51c3ef03b4b
zMishaery <44931500+zMishaeryy@users.noreply.github.com>
2018-11-29 16:56:08 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -373,11 +373,11 @@ client.on('message', message => {
 client.on('message' , message => {
   var prefix = "#";
   if(message.author.bot) return;
-  if(message.content.startsWith(prefix + "ping", "pong")) {
+  if(message.content.startsWith(prefix + "ping")) {
  message.channel.send('Pong...').then((msg) => {
       msg.edit(`\`\`\`javascript\nTime taken: ${msg.createdTimestamp - message.createdTimestamp} ms.\nDiscord API: ${Math.round(client.ping)} ms.\`\`\``);
  })
-  }  
+  }
  });
 
 ////////////////////////////////////////////////////////////////////////////
