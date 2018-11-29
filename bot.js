ac0e806a4d75dd8c951bbe5306f6dac5619ac267
zMishaery <44931500+zMishaeryy@users.noreply.github.com>
2018-11-29 17:05:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -273,6 +273,35 @@ var prefix = "$";
        
 });
 
+//كود فتح وتقفيل الشات
+client.on('message', message => {
+var prefix = "$";
+       if(message.content === prefix + "افتح") {
+                           if(!message.channel.guild) return message.reply('** This command only for servers**');
+
+   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(' **__ظ„ظٹط³ ظ„ط¯ظٹظƒ طµظ„ط§ط­ظٹط§طھ__**');
+              message.channel.overwritePermissions(message.guild.id, {
+            SEND_MESSAGES: false
+
+              }).then(() => {
+                  message.reply("**__طھظ… طھظ‚ظپظٹظ„ ط§ظ„ط´ط§طھ__ :white_check_mark: **")
+              });
+                }
+//FIRE BOT
+    if(message.content === prefix + "اقفل") {
+                        if(!message.channel.guild) return message.reply('** This command only for servers**');
+
+   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('**__ظ„ظٹط³ ظ„ط¯ظٹظƒ طµظ„ط§ط­ظٹط§طھ__**');
+              message.channel.overwritePermissions(message.guild.id, {
+            SEND_MESSAGES: true
+
+              }).then(() => {
+                  message.reply("**__طھظ… ظپطھط­ ط§ظ„ط´ط§طھ__:white_check_mark:**")
+              });
+    }
+       
+});
+
 ////////////////////////////////////////////////////////////////////////////
 
 
