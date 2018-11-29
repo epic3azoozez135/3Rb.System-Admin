ff1f96a59011bd453d863a5687b5c15e5a63783e
zMishaery <44931500+zMishaeryy@users.noreply.github.com>
2018-11-29 14:30:15 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -130,6 +130,22 @@ message.guild.members.forEach( member => {
 }
 });
 
+//كود البانده اقل من شهر
+const moment = require("moment")
+client.on("guildMemberAdd", m => {
+    if (datediff(parseDate(moment(m.user.createdTimestamp).format('l')), parseDate(moment().format('l'))) < 30) {
+        m.ban();
+    };
+    function parseDate(str) {
+        var mdy = str.split('/');
+        return new Date(mdy[2], mdy[0]-1, mdy[1]);
+    };
+   
+    function datediff(first, second) {
+        return Math.round((second-first)/(1000*60*60*24));
+    };
+});
+
 ////////////////////////////////////////////////////////////////////////////
 
 
@@ -222,123 +238,6 @@ msg.delete();
 }
 });
 
-//كود مسح الشات ( الكلير ) نفس سبيد بوت
-client.on('message', message => {
-  var prefix = "#";
- if(!message.channel.guild) return;
-if(message.content.startsWith(prefix + 'مسح')) {
-if(!message.channel.guild) return message.channel.send('**This Command is Just For Servers**').then(m => m.delete(5000));
-if(!message.member.hasPermission('MANAGE_MESSAGES')) return      message.channel.send('**You Do not have permission** `MANAGE_MESSAGES`' );
-let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
-let request = `Requested By ${message.author.username}`;
-message.channel.send(`**Are You sure you want to clear the chat?**`).then(msg => {
-msg.react('✅')
-.then(() => msg.react('❌'))
-.then(() =>msg.react('✅'))
-
-let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
-let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
-
-let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
-let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
-reaction1.on("collect", r => {
-message.channel.send(`Chat will delete`).then(m => m.delete(5000));
-var msg;
-      msg = parseInt();
-
-    message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
-    message.channel.sendMessage("", {embed: {
-      title: "`` Chat Deleted ``",
-      color: 0x06DF00,
-      footer: {
-
-      }
-    }}).then(msg => {msg.delete(3000)});
-
-})
-reaction2.on("collect", r => {
-message.channel.send(`**Chat deletion cancelled**`).then(m => m.delete(5000));
-msg.delete();
-})
-})
-}
-});
-
 ////////////////////////////////////////////////////////////////////////////
-
-
-
-////////////////////////////////////////////////////////////////////////////
-
-//كود فتح وتقفيل الشات
-client.on('message', message => {
- 
-  if (message.content === "#mc") {
-                      if(!message.channel.guild) return message.reply(' هذا الامر فقط للسيرفرات !!');
-
-if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(' ليس لديك صلاحيات');
-         message.channel.overwritePermissions(message.guild.id, {
-       SEND_MESSAGES: false
-
-         }).then(() => {
-             message.reply("تم تقفيل الشات ✅ ")
-         });
-           }
-if (message.content === "#unmc") {
-  if(!message.channel.guild) return message.reply(' هذا الامر فقط للسيرفرات !!');
-
-if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('ليس لديك صلاحيات');
-         message.channel.overwritePermissions(message.guild.id, {
-       SEND_MESSAGES: true
-
-         }).then(() => {
-             message.reply("تم فتح الشات✅")
-         });
-           }
-
-
-
-});
-
-//كود فتح وتقفيل الشات
-client.on('message', message => {
- 
-  if (message.content === "#افتح") {
-                      if(!message.channel.guild) return message.reply(' هذا الامر فقط للسيرفرات !!');
-
-if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(' ليس لديك صلاحيات');
-         message.channel.overwritePermissions(message.guild.id, {
-       SEND_MESSAGES: false
-
-         }).then(() => {
-             message.reply("تم تقفيل الشات ✅ ")
-         });
-           }
-if (message.content === "#قفل") {
-  if(!message.channel.guild) return message.reply(' هذا الامر فقط للسيرفرات !!');
-
-if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('ليس لديك صلاحيات');
-         message.channel.overwritePermissions(message.guild.id, {
-       SEND_MESSAGES: true
-
-         }).then(() => {
-             message.reply("تم فتح الشات✅")
-         });
-           }
-
-
-
-});
-
-////////////////////////////////////////////////////////////////////////////
-
-
-
-
-
-
-
-
-
 
 
