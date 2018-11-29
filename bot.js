843697cc5cf21e347fe8712544ba9797d5859e31
zMishaery <44931500+zMishaeryy@users.noreply.github.com>
2018-11-29 16:33:08 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -265,7 +265,6 @@ msg.delete();
 //كود لانشاء روم صوتي
   client.on('message', message => {
     if (message.content.startsWith(prefix+"voice Room")) {
-         if (message.content.startsWith(prefix+"روم صوتي")) {
         var args = message.content.split(" ").slice(1);
         var argrst = args.join(' ');
                     message.guild.createChannel(`${argrst}`,'voice')
