9d1cbdac74477583bdd38e0fc4fde1204464b519
zMishaery <44931500+zMishaeryy@users.noreply.github.com>
2018-11-29 17:29:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -627,9 +627,48 @@ if(message.content.startsWith(prefix + 'nic')) {
 
 
 
+////////////////////////////////////////////////////////////////////////////
 
+//كود الريبورت او التبليغ على احد
+client.on('message', function(message) {
+    if(message.content.startsWith(prefix + "report")) {
+        let messageArgs = message.content.split(" ").slice(1).join(" ");
+        let messageReason = message.content.split(" ").slice(2).join(" ");
+        if(!messageReason) return message.reply("**# Specify a reason!**");
+    let mUser = message.mentions.users.first();
+    if(!mUser) return message.channel.send("Couldn't find user.");
+    let Rembed = new Discord.RichEmbed()
+    .setTitle("`New Report!`")
+    .setThumbnail(message.author.avatarURL)
+    .addField("**# - Reported User:**",mUser,true)
+    .addField("**# - Reported User ID:**",mUser.id,true)
+    .addField("**# - Reason:**",messageReason,true)
+    .addField("**# - Channel:**",message.channel,true)
+    .addField("**# - Time:**",message.createdAt,true)
+    .setFooter("لو ان الابلاغ فيه مزح راح يتعرض صاحب الابلاغ لقوبات")
+message.channel.send(Rembed)
+message.channel.send("__Are you sure you want to send this to the Server owner??__").then(msg => {
+    msg.react("✅")
+    msg.react("❌")
+.then(() => msg.react('❌'))
+.then(() =>msg.react('✅'))
+let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
+let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
 
+let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
+let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
+reaction1.on("collect", r => {
+    message.guild.owner.send(Rembed)
+    message.reply("**# - Done! 🎇**");
+})
+reaction2.on("collect", r => {
+    message.reply("**# - Canceled!**");
+})
+})
+}
+});
 
+////////////////////////////////////////////////////////////////////////////
 
 
 
@@ -751,53 +790,11 @@ if(message.content.startsWith(prefix + 'nic')) {
 
 
 
+
+
 ////////////////////////////////////////////////////////////////////////////
 
 //يقول كم البوت شغال
-client.on('message', message => {
-var prefix = "#" // البريفكس
-     if (message.author.bot) return;
-if (message.content.startsWith(prefix + "uptime")) { // الامر
-    let uptime = client.uptime;
- 
-    let days = 0;
-    let hours = 0;
-    let minutes = 0;
-    let seconds = 0;
-    let notCompleted = true;
- 
-    while (notCompleted) {
- 
-        if (uptime >= 8.64e+7) {
- 
-            days++;
-            uptime -= 8.64e+7;
- 
-        } else if (uptime >= 3.6e+6) {
- 
-            hours++;
-            uptime -= 3.6e+6;
- 
-        } else if (uptime >= 60000) {
- 
-            minutes++;
-            uptime -= 60000;
- 
-        } else if (uptime >= 1000) {
-            seconds++;
-            uptime -= 1000;
- 
-        }
- 
-        if (uptime < 1000)  notCompleted = false;
- 
-    }
- 
-    message.channel.send("" + ${days} days, ${hours} hrs, ${minutes} , ${seconds} sec+ "");
- 
-}
-});
-
 
 
 ////////////////////////////////////////////////////////////////////////////
