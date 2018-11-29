51c5edf0efcdce012d50ab3f547ac1f9e672f3a9
zMishaery <44931500+zMishaeryy@users.noreply.github.com>
2018-11-29 17:15:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -250,24 +250,24 @@ var prefix = "$";
        if(message.content === prefix + "cl") {
                            if(!message.channel.guild) return message.reply('** This command only for servers**');
 
-   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(' **__ظ„ظٹط³ ظ„ط¯ظٹظƒ طµظ„ط§ط­ظٹط§طھ__**');
+   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(' **__تـم الأن أقـفـال الـشـات مـؤقـت__**');
               message.channel.overwritePermissions(message.guild.id, {
             SEND_MESSAGES: false
 
               }).then(() => {
-                  message.reply("**__طھظ… طھظ‚ظپظٹظ„ ط§ظ„ط´ط§طھ__ :white_check_mark: **")
+                  message.reply("**__تـم الأن أقـفـال الـشـات مـؤقـت__ :white_check_mark:**")
               });
                 }
 //FIRE BOT
     if(message.content === prefix + "op") {
                         if(!message.channel.guild) return message.reply('** This command only for servers**');
 
-   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('**__ظ„ظٹط³ ظ„ط¯ظٹظƒ طµظ„ط§ط­ظٹط§طھ__**');
+   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('**__تـم فـتـح الـشـات وشـكـرا لـكـم__**');
               message.channel.overwritePermissions(message.guild.id, {
             SEND_MESSAGES: true
 
               }).then(() => {
-                  message.reply("**__طھظ… ظپطھط­ ط§ظ„ط´ط§طھ__:white_check_mark:**")
+                  message.reply("**__تـم فـتـح الـشـات وشـكـرا لـكـم__:white_check_mark:**")
               });
     }
        
