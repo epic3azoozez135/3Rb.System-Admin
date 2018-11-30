5e0724b90be30712fb307d7ecfc426b15db42a6e
zMishaery <44931500+zMishaeryy@users.noreply.github.com>
2018-11-30 14:24:50 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -361,7 +361,23 @@ client.on("message", (message) => { // Last Code
 
 
 
-
+client.on('guildMemberAdd', msg => { 
+    var embed = new Discord.RichEmbed()
+    .setAuthor(msg.user.username, msg.user.avatarURL)
+    .setThumbnail(msg.user.avatarURL)
+    .setImage('https://cdn.discordapp.com/attachments/491407337224011796/518024359731003394/welcome.png')     
+    .setTitle('New Member!')
+    .setDescription('Welcome To server')
+    .addField('**ID Member:',"" +  msg.user.id, true)
+    .addField('**Tag Member**', msg.user.discriminator, true)
+    .addField('**Member Created At', msg.user.createdAt, true)
+    .addField(' 👤   You Number',`**[ ${msg.guild.memberCount} ]**`,true)
+    .setColor('GREEN')
+    .setFooter(msg.guild.name, msg.guild.iconURL, true)
+    var channel = msg.guild.channels.find('name', 'chat')         
+    if (!channel) return;
+    channel.send({embed : embed});
+    });
 
 
 
