45357e7a834c5b6e63219e9272694c647b5f9122
zMishaery <44931500+zMishaeryy@users.noreply.github.com>
2018-12-01 20:17:16 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const RichEmbed = require("discord.js");
+const fs = require('fs')
 const { Client, Util } = require('discord.js');
 const client = new Discord.Client();
 const prefix = "#";
