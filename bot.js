b6f950f5b98afb032d8b61826e18367003e21101
zMishaery <44931500+zMishaeryy@users.noreply.github.com>
2018-12-03 21:35:38 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1215,3 +1215,29 @@ client.on('message', message => {
 
 
 
+client.on('message', message => {
+              if(!message.channel.guild) return;
+    var prefix = "#";
+    if(message.content.startsWith(prefix + 'oobc')) {
+    if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
+  if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
+    let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
+    let copy = "alpha codes";
+    let request = `Requested By ${message.author.username}`;
+    if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
+    msg.react('✅')
+    .then(() => msg.react('❌'))
+    .then(() =>msg.react('✅'))
+
+    let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
+    let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
+
+    let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
+    let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
+    reaction1.on("collect", r => {
+    message.channel.send(`☑ | تـم أرسال الـبـردكـاس لـي ${message.guild.members.size} عـضـو`).then(m => m.delete(5000));
+    message.guild.members.forEach(m => {
+
+     
+     
+
