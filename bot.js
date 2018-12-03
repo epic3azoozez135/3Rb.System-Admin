edd5b86f60e3d12beb7bea0a9b666ae584f69042
zMishaery <44931500+zMishaeryy@users.noreply.github.com>
2018-12-03 21:49:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1040,6 +1040,83 @@ client.on('message' , message => {
 ////////////////////////////////////////////////////////////////////////////
 
 //قسم أكواد الالعاب وا الخ . .
+const Sra7a = [
+            'صراحه  |  صوتك حلوة؟',
+            'صراحه  |  التقيت الناس مع وجوهين؟',
+            'صراحه  |  شيء وكنت تحقق اللسان؟',
+            'صراحه  |  أنا ��خص ضعيف عندما؟',
+            'صراحه  |  هل ترغب في إظهار حبك ومرفق لشخص أو رؤية هذا الضعف؟',
+            'صراحه  |  يدل على أن الكذب مرات تكون ضرورية شي؟',
+            'صراحه  |  أشعر بالوحدة على الرغم من أنني تحيط بك كثيرا؟',
+            'صراحه  |  كيفية الكشف عن من يكمن عليك؟',
+            'صراحه  |  إذا حاول شخص ما أن يكرهه أن يقترب منك ويهتم بك تعطيه فرصة؟',
+            'صراحه  |  أشجع شيء حلو في حياتك؟',
+            'صراحه  |  طريقة جيدة يقنع حتى لو كانت الفكرة خاطئة" توافق؟',
+            'صراحه  |  كيف تتصرف مع من يسيئون فهمك ويأخذ على ذهنه ثم ينتظر أن يرفض؟',
+            'صراحه  |  التغيير العادي عندما يكون الشخص الذي يحبه؟',
+            'صراحه  |  المواقف الصعبة تضعف لك ولا ترفع؟',
+            'صراحه  |  نظرة و يفسد الصداقة؟',
+            'صراحه  |  ‏‏إذا أحد قالك كلام سيء بالغالب وش تكون ردة فعلك؟',
+            'صراحه  |  شخص معك بالحلوه والمُره؟',
+            'صراحه  |  ‏هل تحب إظهار حبك وتعلقك بالشخص ��م ترى ذلك ضعف؟',
+            'صراحه  |  تأخذ بكلام اللي ينصحك ولا تسوي اللي تبي؟',
+            'صراحه  |  وش تتمنى الناس تعرف عليك؟',
+            'صراحه  |  ابيع المجرة عشان؟',
+            'صراحه  |  أحيانا احس ان الناس ، كمل؟',
+            'صراحه  |  مع مين ودك تنام اليوم؟',
+            'صراحه  |  صدفة العمر الحلوة هي اني؟',
+            'صراحه  |  الكُره العظيم دايم يجي بعد حُب قوي " تتفق؟',
+            'صراحه  |  صفة تحبها في نفسك؟',
+            'صراحه  |  ‏الفقر فقر العقول ليس الجيوب " ، تتفق؟',
+            'صراحه  |  تصلي صلواتك الخمس كلها؟',
+            'صراحه  |  ‏تجامل أحد على راحتك؟',
+            'صراحه  |  اشجع شيء سويتة بحياتك؟',
+            'صراحه  |  وش ناوي تسوي اليوم؟',
+            'صراحه  |  وش شعورك لما تشوف المطر؟',
+            'صراحه  |  غيرتك هاديه ولا تسوي مشاكل؟',
+            'صراحه  |  ما اكثر شي ندمن عليه؟',
+            'صراحه  |  اي الدول تتمنى ان تزورها؟',
+            'صراحه  |  متى اخر مره بكيت؟',
+            'صراحه  |  تقيم حظك ؟ من عشره؟',
+            'صراحه  |  هل تعتقد ان حظك سيئ؟',
+            'صراحه  |  شـخــص تتمنــي الإنتقــام منـــه؟',
+            'صراحه  |  كلمة تود سماعها كل يوم؟',
+            'صراحه  |  **هل تُتقن عملك أم تشعر بالممل؟',
+            'صراحه  |  هل قمت بانتحال أحد الشخصيات لتكذب على من حولك؟',
+            'صراحه  |  متى آخر مرة قمت بعمل مُشكلة كبيرة وتسببت في خسائر؟',
+            'صراحه  |  ما هو اسوأ خبر سمعته بحياتك؟',
+            '‏صراحه | هل جرحت شخص تحبه من قبل ؟',
+            'صراحه  |  ما هي العادة التي تُحب أن تبتعد عنها؟',
+            '‏صراحه | هل تحب عائلتك ام تكرههم؟',
+            '‏صراحه  |  من هو الشخص الذي يأتي في قلبك بعد الله – سبحانه وتعالى- ورسوله الكريم – صلى الله عليه وسلم؟',
+            '‏صراحه  |  هل خجلت من نفسك من قبل؟',
+            '‏صراحه  |  ما هو ا الحلم  الذي لم تستطيع ان تحققه؟',
+            '‏صراحه  |  ما هو الشخص الذي تحلم به كل ليلة؟',
+            '‏صراحه  |  هل تعرضت إلى موقف مُ��رج جعلك تكره صاحبهُ؟',
+             '‏صراحه  |  هل قمت بالبكاء أمام من تُحب؟',
+            '‏صراحه  |  ماذا تختار حبيبك أم صديقك؟',
+            '‏صراحه  | هل حياتك سعيدة أم حزينة؟',
+            'صراحه  |  ما هي أجمل سنة عشتها بحياتك؟',
+            '‏صراحه  |  ما هو عمرك الحقيقي؟',
+            '‏صراحه  |  ما اكثر شي ندمن عليه؟',
+            'صراحه  |  ما هي أمنياتك المُستقبلية؟‏',
+       ]
+          client.on('message', message => {
+			  	var prefix = "*"
+        if (message.content.startsWith(prefix + 'صراحه')) {
+            if(!message.channel.guild) return message.reply('** This command only for servers **');
+         var client= new Discord.RichEmbed()
+         .setTitle("Alpha")
+         .setColor('RANDOM')
+         .setDescription(`${Sra7a[Math.floor(Math.random() * Sra7a.length)]}`)
+         .setImage("https://cdn.discordapp.com/attachments/371269161470525444/384103927060234242/125.png")
+                         .setTimestamp()
+       
+          message.channel.sendEmbed(client);
+          message.react("??")
+        }
+       });
+       
 
 ////////////////////////////////////////////////////////////////////////////
 
@@ -1047,12 +1124,85 @@ client.on('message' , message => {
 
 ////////////////////////////////////////////////////////////////////////////
 
-//كود اسئلة لعبة فورت نايت
+//كود لعيت عواصم
+client.on('message', message => {
+if (!points[message.author.id]) points[message.author.id] = {
+    points: 50,
+  };
+if (message.content.startsWith(prefix + 'عواصم')) { 
+    if(!message.channel.guild) return message.reply('**هذا الأمر للسيرفرات فقط**').then(m => m.delete(3000));
 
+const type = require('./3wasem/3wasem.json'); 
+const item = type[Math.floor(Math.random() * type.length)]; 
+const filter = response => { 
+    return item.answers.some(answer => answer.toLowerCase() === response.content.toLowerCase());
+};
+message.channel.send('**اديك 15 ثاني�� لتوجد العاصمه الصحيحه**').then(msg => {
+    let embed = new Discord.RichEmbed()
+    .setColor('#000000')
+    .setFooter("عواصم  | Legend GaMerZ Bot", 'https://cdn.discordapp.com/avatars/439427357175185408/3eb163b7656922ebc9e90653d50231f1.png?size=2048')
+    .setDescription(`**اكتب عاصمه: ${item.type}**`)
+
+    msg.channel.sendEmbed(embed).then(() => {
+        message.channel.awaitMessages(filter, { maxMatches: 1, time: 15000, errors: ['time'] })
+        .then((collected) => {
+        message.channel.send(`${collected.first().author} ✅ **الاجابه صحيحه**`); 
+
+        console.log(`[Typing] ${collected.first().author} typed the word.`);
+            let won = collected.first().author; 
+            points[won.id].points++;
+          })
+          .catch(collected => { 
+            message.channel.send(`:x: **لا يوجد احد كتب الاجابه الصحيحه**`);
+            console.log(`[Typing] ماحد قال الاجابه `);
+          })
+        })
+    })
+}
+});
+
+//============================ الجيسون
+
+[
+    {
+        "type": "العراق",
+        "answers": ["بغداد"]
+    },
+    {
+        "type": "مصر",
+        "answers": ["القاهرة"]
+    },
+    {
+        "type": "سوريا",
+        "answers": ["دمشق"]
+    },
+    {
+        "type": "السعودية",
+        "answers": ["الرياض"]
+    },
+    {
+        "type": "اليمن",
+        "answers": ["صنعاء"]
+    },
+    {
+        "type": "السودان",
+        "answers": ["الخرطوم"]
+    },
+    {
+        "type": "الاردن",
+        "answers": ["عمان"]
+    },
+    {
+        "type": "ليبيا",
+        "answers": ["طرابلس"]
+    },
+    {
+        "type": "البحرين",
+        "answers": ["المنامة"]
+    }
+]
 
 ////////////////////////////////////////////////////////////////////////////
-
-
 
 
 
@@ -1073,8 +1223,6 @@ client.on('message' , message => {
 
 
 ////////////////////////////////////////////////////////////////////////////
-
-
 
 
 
@@ -1215,33 +1363,6 @@ client.on('message', message => {
 
 
 
-//برد كاس امبد
-client.on('message', message => {
-              if(!message.channel.guild) return;
-    var prefix = "#";
-    if(message.content.startsWith(prefix + 'mbc')) {
-    if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
-  if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
-    let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
-    let copy = "alpha codes";
-    let request = `Requested By ${message.author.username}`;
-    if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
-    msg.react('✅')
-    .then(() => msg.react('❌'))
-    .then(() =>msg.react('✅'))
 
-    let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
-    let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
-
-    let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
-    let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
-    reaction1.on("collect", r => {
-    message.channel.send(`☑ | تـم أرسال الـبـردكـاس لـي ${message.guild.members.size} عـضـو`).then(m => m.delete(5000));
-    message.guild.members.forEach(m => {
-    })
-    })
-    }
-    });
-     
 
      
