1efb96497cb96367055cd585b11816308e4b8c67
zMishaery <44931500+zMishaeryy@users.noreply.github.com>
2018-12-03 18:24:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -366,14 +366,9 @@ client.on("message", (message) => { // Last Code
 
 
 
-client.on('guildMemberAdd', member => {
-  member.guild.fetchInvites().then(guildInvites => {
-    const ei = invites[member.guild.id];
-    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
-    const inviter = client.users.get(invite.inviter.id);
-    const stewart = member.guild.channels.find("name", "أسم الشات");
-     stewart.send(`<@${member.user.id}> Join.By <@${inviter.id}>`);
-  });
+
+
+
 
 
 
