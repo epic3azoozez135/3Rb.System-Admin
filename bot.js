8a62d2854fc63c17067eeb1c3b2e22f2ec7af835
zMishaery <44931500+zMishaeryy@users.noreply.github.com>
2018-12-03 16:54:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -928,7 +928,7 @@ if(message.content.startsWith(prefix + 'nic')) {
   if(!message.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS','MANAGE_ROLES_OR_PERMISSIONS'])) return message.reply(':x: **ليس معي الصلاحيات الكافية**');
   var args = message.content.split(' ').slice(1).join(' ');
   if(args && !args.includes(0)) return message.channel.send(':negative_squared_cross_mark: » فشل اعداد الروم الصوتي .. __يجب عليك كتابة 0 في اسم الروم__');
-  if(!args) args = `VoiceOnline: [ ${message.guild.members.filter(s => s.voiceChannel).size} ]`;
+  if(!args) args = `# - 3Rb.Online : { ${message.guild.members.filter(s => s.voiceChannel).size} }`;
   message.channel.send(':white_check_mark: » تم عمل الروم الصوتي بنجاح');
   message.guild.createChannel(`${args.replace(0, message.guild.members.filter(s => s.voiceChannel).size)}`, 'voice').then(c => {
     c.overwritePermissions(message.guild.id, {
